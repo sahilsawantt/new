7156bc6c5bea62b505fe739f81fbc54cacdadf7c
sahilsawantt <sawantsahil591@gmail.com>
2025-06-05 09:14:44 +0530

    s

--- cargame2d/static/script.js
+++ cargame2d/static/script.js
@@ -34,7 +34,7 @@ let player = { speed: 5, score: 0, start: false };
 let keys = {};
 let scoreCounter = 0;
 let speedincrement = 0.01;
-let maxspeed = 15;
+let maxspeed = 5;
 
 // 🎵 Play background music on first user click
 // window.addEventListener("click", () => {
